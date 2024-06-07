9a095dc9006ad6a0e250abea41195650a002bbe2
Anupta Islam <anuptaislam33@gmail.com>
2024-06-07 03:34:39 -0400

    registration screen + endpoint connection for sign up + sign in UI

--- comend-crm/src/components/Landing.tsx
+++ comend-crm/src/components/Landing.tsx
@@ -1,11 +1,27 @@
+"use client"; // This file is a client component
+
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import { useState } from 'react'
 import SignInForm from './SignInForm';
+import Dashboard from './Dashboard';
 import { ProfileForm } from './ProfileForm';
+import Link from 'next/link';
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Marketplace', href: '#' },
+    { name: 'Company', href: '#' },
+]
 
 export default function Landing() {
+    const [signedIn, setSignedIn] = useState(false);
     const [userRegistering, setUserRegistering] = useState(false);
     const [revisitingUser, setRevisitingUser] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
 
     const defaultLandingLeftSide = () => (
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
@@ -14,6 +30,79 @@ export default function Landing() {
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
             />
+
+            <header className="absolute inset-x-0 top-0 z-50">
+                <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+                    <div className="flex lg:hidden">
+                        <button
+                            type="button"
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="hidden lg:flex lg:gap-x-12">
+                        {navigation.map((item) => (
+                            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                            Log in <span aria-hidden="true">&rarr;</span>
+                        </a>
+                    </div>
+                </nav>
+                <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <div className="fixed inset-0 z-50" />
+                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Your Company</span>
+                                <img
+                                    className="h-8 w-auto"
+                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                    alt=""
+                                />
+                            </a>
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    {navigation.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        >
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
+                                <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        Log in
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </DialogPanel>
+                </Dialog>
+            </header>
 
             <div className="mt-24 sm:mt-32 lg:mt-16">
                 <a href="#" className="inline-flex space-x-6">
@@ -48,23 +137,18 @@ export default function Landing() {
                         setRevisitingUser(false);
                     }}
                     className="text-sm font-semibold leading-6 text-gray-900">
-                    Register <span aria-hidden="true">→</span>
+                    <Link href="/registration">
+                        Register <span aria-hidden="true">→</span>
+                    </Link>
                 </button>
             </div>
         </div>
     );
 
-    const signInForm = () => {
+    const LandingContent = () => {
         return (
-            <div>
-
-            </div>
-        )
-    }
-
-    return (
-        <div className="relative isolate overflow-hidden bg-white">
-            {/* <svg
+            <div className="relative isolate overflow-hidden bg-white">
+                {/* <svg
                 className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
                 aria-hidden="true"
             >
@@ -82,24 +166,29 @@ export default function Landing() {
                 </defs>
                 <rect width="100%" height="100%" strokeWidth={0} fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" />
             </svg> */}
-            <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-                {/* {revisitingUser ? <SignInForm setRevisitingUser={setRevisitingUser} /> : defaultLandingLeftSide()} */}
-                {revisitingUser ? <ProfileForm setRevisitingUser={setRevisitingUser} /> : defaultLandingLeftSide()}
+                <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+                    {/* {revisitingUser ? <SignInForm setRevisitingUser={setRevisitingUser} /> : defaultLandingLeftSide()} */}
+                    {revisitingUser ? <ProfileForm setRevisitingUser={setRevisitingUser} /> : defaultLandingLeftSide()}
 
-                <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-                    <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-                        <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                            <img
-                                src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
-                                alt="App screenshot"
-                                width={2432}
-                                height={1442}
-                                className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
-                            />
+                    <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                        <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                                <img
+                                    src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                                    alt="App screenshot"
+                                    width={2432}
+                                    height={1442}
+                                    className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        )
+    }
+
+    return (
+        signedIn ? <Dashboard /> : <LandingContent />
     )
 }
